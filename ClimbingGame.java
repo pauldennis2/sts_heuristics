a35b3e2a63e5f9ba07268e6ac5110746cb8d2220
pd236m <pd236m@att.com>
2018-05-25 15:12:24 -0400

    Refactored static functions from ClimbingGame into their own class

--- ClimbingGame.java
+++ ClimbingGame.java
@@ -4,19 +4,11 @@
  */
 package sts_heuristics;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class ClimbingGame {
@@ -30,25 +22,12 @@ public class ClimbingGame {
 	
 	static Map<String, Card> newCardOptions;
 	static Map<String, HeroAction> advUpgradeOptions;
-	static Map<AdaptiveStrategy, Double> strategyAverageAttainment;
-	
-	static Map<AdaptiveStrategy, List<Integer>> strategyToLevelsAttainedMap;
-	
-	static HallOfFame hallOfFame;
 	
 	//0 - used for highest level output
 	//1 - used for things that occur 1-2 times per game
 	//2 - used for things that occur timers per round
 	public static int OUTPUT_LEVEL = 4;
 	private static boolean init = false;
-	static AtomicLong timeOutCount;
-	static AtomicLong deathCount;
-	static AtomicLong gameCount;
-	
-	static long timeSpentOnDeathGames = 0;
-	static long timeSpentOnTimeOutGames = 0;
-	
-	private static ExecutorService threadService;
 	
 	public ClimbingGame () {
 		if (!init) {
@@ -68,15 +47,6 @@ public class ClimbingGame {
 	}
 	
 	public static void staticInit () {
-		hallOfFame = new HallOfFame();
-		strategyAverageAttainment = new ConcurrentHashMap<>();
-		strategyToLevelsAttainedMap = new ConcurrentHashMap<>();
-		
-		deathCount = new AtomicLong(0);
-		timeOutCount = new AtomicLong(0);
-		
-		gameCount = new AtomicLong(0);
-		
 		advUpgradeOptions = new HashMap<>();
 		//Remove the least powerful card from the deck
 		advUpgradeOptions.put("removeCard", hero -> {
@@ -175,258 +145,8 @@ public class ClimbingGame {
 		});
 		init = true;
 	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		long masterStartTime = System.currentTimeMillis();
-		staticInit();
-		//breedingMethod1();
-		//getGrittyHallOfFameData();
-		for (int i = 0; i < 3; i++) {
-			new ClimbingGame(new AdaptiveStrategy(new Hero())).playGame();
-		}
-		hallOfFame.close();
-		long masterEndTime = System.currentTimeMillis();
-		
-		System.out.println("Whole process took: " + (masterEndTime - masterStartTime) + " milliseconds.");
-		
-		System.out.println("Death count = " + deathCount);
-		System.out.println("Time out count = " + timeOutCount);
-		
-		System.out.println("Game count = " + gameCount);
-	}
-	
-	public static void breedingMethod1 () {
-		long startTime;
-		long endTime;
-		seedHallOfFame();
-		
-		startTime = System.currentTimeMillis();
-		breedStrategies(hallOfFame.getPotentials(), 3);
-		endTime = System.currentTimeMillis();
-		System.out.println("Breeding took: " + (endTime - startTime));
-		
-		startTime = System.currentTimeMillis();
-		breedStrategies(hallOfFame.getFamers(), 50);
-		endTime = System.currentTimeMillis();
-		System.out.println("Breeding took: " + (endTime - startTime));
-		
-		startTime = System.currentTimeMillis();
-		deepBreedStrategies(hallOfFame.getPotentials(), 2);
-		endTime = System.currentTimeMillis();
-		System.out.println("Breeding took: " + (endTime - startTime));
-		
-		deepBreedStrategies(hallOfFame.getFamers(), 10);
-		crossBreedStrategies(hallOfFame.getFamers(), 1);
-	}
 
-	/* This method takes the Hall of Fame strategies, runs them 500 times each,
-	 * sums up their level attained counts (i.e. "Died on level 15 3 times, died on level 16 2 times"),
-	 * and writes this info to data/gritty/<currentTimeMillis>/<strategyName>.csv
-	 * for use in excel spreadsheet and the like.
-	 */
-	public static void getGrittyHallOfFameData () {
-		long startTime = System.currentTimeMillis();
-		System.out.println("Getting nitty gritty details on Hall of Fame top 20.");
-		strategyToLevelsAttainedMap = new HashMap<>();
-		//Map<AdaptiveStrategy, Map<Integer, Integer>> strategiesToCountMaps = new HashMap<>();
-		String directory = "data/gritty/" + System.currentTimeMillis() + "/";
-		new File(directory).mkdirs();
-		for (AdaptiveStrategy strategy : hallOfFame.getFamers()) {
-			for (int i = 0; i < 1000; i++) {
-				new ClimbingGame(strategy).playGame();
-			}
-			List<Integer> levelsAttained = strategyToLevelsAttainedMap.get(strategy);
-			Map<Integer, Integer> levelsAttainedCountMap = new HashMap<>();
-			for (Integer level : levelsAttained) {
-				if (levelsAttainedCountMap.containsKey(level)) {
-					levelsAttainedCountMap.put(level, levelsAttainedCountMap.get(level) + 1);
-				} else {
-					levelsAttainedCountMap.put(level, 1);
-				}
-			}
-			String fileName = directory + strategy.getName() + ".csv";
-			try (FileWriter fileWriter = new FileWriter(new File(fileName))){
-				List<Integer> levelsAttainedKeys = levelsAttainedCountMap.keySet().stream().collect(Collectors.toList());
-				Collections.sort(levelsAttainedKeys);
-				for (Integer level : levelsAttainedKeys) {
-					fileWriter.write(level + ", " + levelsAttainedCountMap.get(level));
-					fileWriter.write("\n");
-				}
-				fileWriter.close();
-			} catch (IOException ex) {
-				System.err.println("!!Problem writing to file: " + fileName);
-			}
-		}
-		long endTime = System.currentTimeMillis();
-		long duration = endTime - startTime;
-		System.out.println("Took " + duration + " milliseconds to run 20 strategies 1000 times.");
-		System.out.println("Average time to run a game: " + (double)duration / 20000.0);
-		System.out.println("Finished getting gritty.");
-	}
-	
-	//Create 500 new strategies and run them 500 times each	
-	public static void seedHallOfFame () {
-		System.out.println("Seeding Hall of Fame by running 500 strategies 500 times.");
-		System.out.println("(Process should take < 20 seconds.)");
-		List<AdaptiveStrategy> newStrats = new ArrayList<>();
-		for (int i = 0; i < 500; i++) {
-			newStrats.add(new AdaptiveStrategy(new Hero()));
-		}
-		runStrategies(newStrats, 500);
-		hallOfFame.addPotentialMembers(newStrats);
-	}
-	
-	public static void breedStrategies (List<AdaptiveStrategy> strategies, int numChildren) {
-		System.out.println("Received " + strategies.size() + " strategies.");
-		System.out.println("Breeding by giving each " + numChildren + " children.");
-		List<AdaptiveStrategy> children = new ArrayList<>();
-		for (AdaptiveStrategy strategy : strategies) {
-			for (int i = 0; i < numChildren; i++) {
-				children.add(strategy.tweak());
-			}
-		}
-		runStrategies(children, 500);
-		Collections.sort(children);
-		System.out.println("highest attainment = " + children.get(0).averageLevelAttained);
-		
-		hallOfFame.addPotentialMembers(children);
-	}
-	
-	public static void deepBreedStrategies (List<AdaptiveStrategy> strategies, int numGreatGrandchildren) {
-		System.out.println("Received " + strategies.size() + " strategies.");
-		System.out.println("Deep breeding each by giving it " + numGreatGrandchildren + " great-great-grandchildren.");
-		List<AdaptiveStrategy> children = new ArrayList<>();
-		for (AdaptiveStrategy strategy : strategies) {
-			for (int i = 0; i < numGreatGrandchildren; i++) {
-				AdaptiveStrategy grGrGrandChild = strategy.tweak().tweak().tweak().tweak();
-				children.add(grGrGrandChild);
-			}
-		}
-		runStrategies(children, 500);
-		Collections.sort(children);
-		System.out.println("highest attainment = " + children.get(0).averageLevelAttained);
-		
-		hallOfFame.addPotentialMembers(children);
-	}
-	
-	public static void crossBreedStrategies (List<AdaptiveStrategy> strategies, int numChildren) {
-		System.out.println("Received " + strategies.size() + " strategies.");
-		System.out.println("Cross-breeding these strategies by giving each pair " + 2 * numChildren + " children.");
-		int numPairs = strategies.size() * (strategies.size() - 1);
-		System.out.println("This will create " + numPairs * 2 + " strategies.");
-		System.out.println("(Cross-breeding is an O(n^2) operation.)");
-		List<AdaptiveStrategy> children = new ArrayList<>();
-		for (AdaptiveStrategy dad : strategies) {
-			for (AdaptiveStrategy mom : strategies) {
-				if (dad.equals(mom)) {
-					continue;
-				}
-				for (int i = 0; i < numChildren; i++) {
-					children.add(new AdaptiveStrategy(dad, mom));
-				}
-			}
-		}
-		runStrategies(children, 500);
-		Collections.sort(children);
-		System.out.println("highest attainment = " + children.get(0).averageLevelAttained);
-		
-		hallOfFame.addPotentialMembers(children);
-	}
-	
-	public static void runStrategies (List<AdaptiveStrategy> strategies, int numTimes) {
-		threadService = Executors.newCachedThreadPool();
-		for (AdaptiveStrategy strategy : strategies) {
-			threadService.submit(() -> {
-				for (int i = 0; i < numTimes; i++) {
-					new ClimbingGame(strategy).playGame();
-				}
-			});
-		}
-		
-		threadService.shutdown();
-		try {
-			threadService.awaitTermination(240, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			throw new AssertionError(e);
-		}
-		
-		calcAverageAttainment();
-	}
-	
-	/* Test the variability to find out how many runs it takes to get a solid read
-	 * on how successful a given strategy is
-	 * Result: variability ~= 1.3367*runCount^-0.504  . Basically 1.33/sqrt(runCount)
-	 * Variability with runCount 500 = 0.0583 (normal amount)
-	 * Variability with runCount 10000 = 0.01288
-	 */
-	public static void testVariability2 () {
-		Map<Integer, Double> runCountToDifferenceMap = new HashMap<>();
-		for (int runCount = 5; runCount < 150; runCount+= 5) {
-			List<AdaptiveStrategy> strategies = new ArrayList<>();
-			for (int i = 0; i < 1000; i++) {
-				strategies.add(new AdaptiveStrategy(new Hero()));
-			}
-			runStrategies(strategies, runCount);
-			Map<AdaptiveStrategy, Double> initialAverageAttainment = new HashMap<>();
-			for (AdaptiveStrategy strategy : strategies) {
-				initialAverageAttainment.put(strategy, strategy.averageLevelAttained);
-				for (int j = 0; j < runCount; j++) {
-					new ClimbingGame(strategy).playGame();
-				}
-			}
-			calcAverageAttainment();
-			double totalDifference = 0.0;
-			for (AdaptiveStrategy strategy : strategies) {
-				double difference = Math.abs(strategy.averageLevelAttained - initialAverageAttainment.get(strategy));
-				totalDifference += difference;
-			}
-			System.out.println("Run count = " + runCount);
-			System.out.println("Average difference = " + totalDifference / 1000.0);
-			runCountToDifferenceMap.put(runCount, totalDifference / 1000.0);
-			strategyToLevelsAttainedMap = new HashMap<>();
-		}
-		System.out.println("===");
-		for (Integer runCount : runCountToDifferenceMap.keySet()) {
-			System.out.println(runCount + ", " + runCountToDifferenceMap.get(runCount));
-		}
-	}
-	
-	public static void joyfulDragonTest () {
-		AdaptiveStrategy joyfulDragon = AdaptiveStrategy.buildJoyfulDragon();
-		for (int i = 0; i < 5000; i++) {
-			ClimbingGame game = new ClimbingGame(joyfulDragon);
-			game.playGame();
-		}
-		double av = strategyToLevelsAttainedMap.get(joyfulDragon).stream()
-			.mapToInt(num -> num)
-			.average()
-			.getAsDouble();
-		
-		System.out.println("Average attainment = " + av);
-	}
-		
-	public static void calcAverageAttainment () {
-		strategyToLevelsAttainedMap.keySet().forEach(strat -> {
-			List<Integer> levels = strategyToLevelsAttainedMap.get(strat);
-			//double average = levels.stream().mapToInt(e -> e).average().getAsDouble();
-			int sum = 0;
-			for (Integer i : levels) {
-				sum += i;
-			}
-			double average = (double) sum / (double) levels.size();
-			int count = levels.size();
-			
-			if (OUTPUT_LEVEL >= 2) {
-				System.out.println("Strategy: " + strat);
-				System.out.println("Average Level Reached: " + average);
-				System.out.println("Occurances: " + count);
-			}
-			strategyAverageAttainment.put(strat, average);
-			strat.averageLevelAttained = average;
-		});
-	}
-	public void playGame () {
-		gameCount.incrementAndGet();
+	public int playGame () {
 		boolean keepGoing = true;
 		while (keepGoing) {
 			if (OUTPUT_LEVEL >= 2) {
@@ -474,7 +194,6 @@ public class ClimbingGame {
 						System.out.println("Hero died on level " + level + ".");
 					}
 					keepGoing = false;
-					deathCount.incrementAndGet();
 					break; //Out of the individual round loop
 				}
 				if (roundCount >= 30) {
@@ -482,7 +201,6 @@ public class ClimbingGame {
 						System.out.println("Timed out on combat after 30+ rounds. Hero loses.");
 					}
 					keepGoing = false;
-					timeOutCount.incrementAndGet();
 					break;
 				}
 				roundCount++;
@@ -499,16 +217,10 @@ public class ClimbingGame {
 				
 				makeUpgradeDecision();
 			} else {
-				List<Integer> levelsAttained;
-				if (strategyToLevelsAttainedMap.containsKey(hero.getStrategy())) {
-					levelsAttained = strategyToLevelsAttainedMap.get(hero.getStrategy());
-				} else {
-					levelsAttained = Collections.synchronizedList(new ArrayList<>());
-				}
-				levelsAttained.add(level);
-				strategyToLevelsAttainedMap.put(hero.getStrategy(), levelsAttained);
+				return level;
 			}
 		}//End while(keepGoing)
+		throw new AssertionError("Should not reach this point.");
 	}
 	
 	public void makeUpgradeDecision () {
